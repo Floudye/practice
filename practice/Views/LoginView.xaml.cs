e93f2ad546fd5eb7b9b4f2741aa4c764438a3efb
Floudye <Floudye@git.ru>
2024-03-14 17:42:56 +0800

    Добавьте файлы проекта.

---
+++ practice/Views/LoginView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practice.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        private string Password;
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new practiceEntities())
+            {
+                var user = context.Clients
+                    .FirstOrDefault(u => u.Username == UsernameTextBox.Text
+                    && u.Password == Password);
+
+                if (user != null)
+                {
+                    MainView main = new MainView();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль.");
+                }
+            }
+        }
+
+
+        private void SignUp_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SignUpView signUp = new SignUpView();
+            signUp.Show();
+        }
+
+        private void PasswordBox1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)sender;
+            Password = passwordBox.Password;
+        }
+    }
+}
---
+++ practice/Views/SignUpView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practice.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterView.xaml
+    /// </summary>
+    public partial class SignUpView : Window
+    {
+        private string Password;
+        public SignUpView()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_signUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UsernameTextBox.Text) || string.IsNullOrWhiteSpace(PasswordTextBox.Password) ||
+            string.IsNullOrWhiteSpace(FirstNameTextBox.Text) || string.IsNullOrWhiteSpace(SecondNameTextBox.Text) ||
+            string.IsNullOrWhiteSpace(PatronymicTextBox.Text) || string.IsNullOrWhiteSpace(PhoneNumberTextBox.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return; 
+            }
+
+            using (var context = new practiceEntities())
+            {
+                var user = new Clients
+                {
+                    Username = UsernameTextBox.Text,
+                    Password = Password,
+                    FirstName = FirstNameTextBox.Text,
+                    SecondName = SecondNameTextBox.Text,
+                    Patronymic = PatronymicTextBox.Text,
+                    PhoneNumber = PhoneNumberTextBox.Text,
+                };
+                context.Clients.Add(user);
+                context.SaveChanges();
+            }
+
+            MessageBox.Show("Пользователь зарегистрирован.");
+            this.Close();
+        }
+        private void PasswordBox1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)sender;
+            Password = passwordBox.Password;
+        }
+    }
+}
